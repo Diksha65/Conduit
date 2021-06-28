30b8ebf16d359359379137ae9ce2391cb2dfc890
Diksha Agarwal <agarwaldiksha6596@gmail.com>
2021-06-28 18:03:17 +0530

    Code format

--- app/src/main/java/com/project/conduit/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/project/conduit/ui/gallery/GalleryFragment.kt
@@ -7,20 +7,20 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.ViewModelProvider
 import com.project.conduit.R
 
-class GalleryFragment : Fragment() {
+class GalleryFragment() : Fragment() {
 
     private lateinit var galleryViewModel: GalleryViewModel
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
-        galleryViewModel =
-                ViewModelProviders.of(this).get(GalleryViewModel::class.java)
+        galleryViewModel = ViewModelProvider(this).get(GalleryViewModel::class.java)
+
         val root = inflater.inflate(R.layout.fragment_gallery, container, false)
         val textView: TextView = root.findViewById(R.id.text_gallery)
         galleryViewModel.text.observe(viewLifecycleOwner, Observer {
--- app/src/main/java/com/project/conduit/ui/slideshow/SlideshowFragment.kt
+++ app/src/main/java/com/project/conduit/ui/slideshow/SlideshowFragment.kt
@@ -7,7 +7,7 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.ViewModelProvider
 import com.project.conduit.R
 
 class SlideshowFragment : Fragment() {
@@ -15,17 +15,18 @@ class SlideshowFragment : Fragment() {
     private lateinit var slideshowViewModel: SlideshowViewModel
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
-        slideshowViewModel =
-                ViewModelProviders.of(this).get(SlideshowViewModel::class.java)
+        slideshowViewModel = ViewModelProvider(this).get(SlideshowViewModel::class.java)
+
         val root = inflater.inflate(R.layout.fragment_slideshow, container, false)
         val textView: TextView = root.findViewById(R.id.text_slideshow)
         slideshowViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
+
         return root
     }
 }
