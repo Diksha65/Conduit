4c61690b20c870b41bfa1ea72d96fb37474f8864
Diksha Agarwal <agarwaldiksha6596@gmail.com>
2021-06-28 19:32:58 +0530

    Setting up all the fragments finally

--- app/src/main/java/com/project/conduit/MainActivity.kt
+++ app/src/main/java/com/project/conduit/MainActivity.kt
@@ -37,7 +37,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
+                R.id.nav_home, R.id.nav_login, R.id.nav_register, R.id.nav_new_article, R.id.nav_settings
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
